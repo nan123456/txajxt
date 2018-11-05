d694c8a93c011f6cd415a4410bc073b585e7fa7b
Lin971018 <30288831+Lin971018@users.noreply.github.com>
2018-11-05 18:12:24 +0800

    2018.11.5-18点12分

--- txajxtcs/my_plus/my_change_cgxz.php
+++ txajxtcs/my_plus/my_change_cgxz.php
@@ -4,24 +4,23 @@
 	$gcid=$_POST["gcid"];
 	$wxyid=$_POST["wxyid"];
 	
-//	$gcid="661";
-//	$wxyid="338";
+//	$gcid="705";
+//	$wxyid="350";
 	
 	$sqldate="";
-	$sql = "SELECT * FROM 通知单 where 工程id='$gcid' and 危险源id='$wxyid'";
+	$sql = "SELECT * FROM 危险源 where 工程id='$gcid' and id='$wxyid'";
 	$result = $conn->query($sql);
-	$count=mysqli_num_rows($result);	
+//	$count=mysqli_num_rows($result);	
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			$sqldate= $sqldate.'{"检查对象":"'.$row["检查对象"].'"},';
+			$sqldate= $sqldate.'{"检查对象":"'.$row["工程名称"].'"},';
 		 }
 	} else {
 		//echo "0 results";
 	}
 	//echo $sqldate;
 	$jsonresult='success';
-	$otherdate = '{"result":"'.$jsonresult.'",
-				"count":"'.$count.'"
+	$otherdate = '{"result":"'.$jsonresult.'"
 				}';
 				
 	$json = '['.$sqldate.$otherdate.']';
--- txajxtcs/my_plus/my_change_cgxz_1.php
+++ txajxtcs/my_plus/my_change_cgxz_1.php
@@ -2,26 +2,25 @@
 	require("../conn.php");
 
 	$gcid=$_POST["gcid"];
-	$id=$_POST["id"];
+	$wxyid=$_POST["wxyid"];
 	
 //	$gcid="661";
 //	$wxyid="338";
 	
 	$sqldate="";
-	$sql = "SELECT * FROM 危险源 where 工程id='$gcid' and id='$id'";
+	$sql = "SELECT * FROM 危险源 where 工程id='$gcid' and id='$wxyid'";
 	$result = $conn->query($sql);
-	$count=mysqli_num_rows($result);	
+//	$count=mysqli_num_rows($result);	
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			$sqldate= $sqldate.'{"危险源名称":"'.$row["危险源名称"].'","危险源部位":"'.$row["危险源部位"].'"},';
+			$sqldate= $sqldate.'{"危险源名称":"'.$row["危险源名称"].'"},';
 		 }
 	} else {
 		//echo "0 results";
 	}
 	//echo $sqldate;
 	$jsonresult='success';
-	$otherdate = '{"result":"'.$jsonresult.'",
-				"count":"'.$count.'"
+	$otherdate = '{"result":"'.$jsonresult.'"
 				}';
 				
 	$json = '['.$sqldate.$otherdate.']';
